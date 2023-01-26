35b1139dfbce12514fe6c43fc2d2bfa38810af2d
yashashr98 <hryashas9124@gmail.com>
2023-01-26 21:31:16 +0530

    update app.js

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -41,7 +41,7 @@ function App() {
         <>
           <Navbar expand="lg" bg="secondary" variant="dark">
             <Container>
-              <Navbar.Brand href="http://www.dappuniversity.com/bootcamp">
+              <Navbar.Brand href="#">
                 <img src={logo} width="40" height="40" className="" alt="" />
                 &nbsp; Music NFT player
               </Navbar.Brand>
